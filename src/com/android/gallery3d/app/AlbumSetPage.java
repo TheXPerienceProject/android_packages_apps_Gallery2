4389fe206e727922bf4e0662d384472b3bdae4a4
jinwu <jinwu@codeaurora.org>
2018-01-29 15:12:30 +0800

    Don't show Camera Icon when no pictures found in Albums
    Remove the camera icon and show action.
    
    Change-Id: I758ce1ebc4e25128023423c456b59300cd724f29

--- src/com/android/gallery3d/app/AlbumSetPage.java
+++ src/com/android/gallery3d/app/AlbumSetPage.java
@@ -385,11 +385,6 @@ public class AlbumSetPage extends ActivityState implements
         mCameraButton = null;
     }
 
-    private void showCameraButton() {
-        if (mCameraButton == null && !setupCameraButton()) return;
-        mCameraButton.setVisibility(View.VISIBLE);
-    }
-
     private void hideCameraButton() {
         if (mCameraButton == null) return;
         mCameraButton.setVisibility(View.GONE);
@@ -412,7 +407,6 @@ public class AlbumSetPage extends ActivityState implements
                     mShowedEmptyToastForSelf = true;
                     showEmptyAlbumToast(Toast.LENGTH_LONG);
                     mSlotView.invalidate();
-                    showCameraButton();
                 }
                 return;
             }
